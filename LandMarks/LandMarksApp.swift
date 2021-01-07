1587cb8a7fef71ce4f6edf9c93362bd8438296e1
obaya884 <taku3.884@gmail.com>
2021-01-08 01:53:49 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 大林拓実 on 2021/01/08.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
