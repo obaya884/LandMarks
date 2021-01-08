7f2ad64b6a1bb2dd7aa3f98d76a5237f753680e8
obaya884 <taku3.884@gmail.com>
2021-01-08 16:03:50 +0900

    Creating and Combining Views

---
+++ LandMarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by 大林拓実 on 2021/01/08.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ LandMarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  LandMarks
+//
+//  Created by 大林拓実 on 2021/01/08.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
