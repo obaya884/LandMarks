8793ebd03160b3d368573c7de222a2e0be8755cd
obaya884 <taku3.884@gmail.com>
2021-01-22 01:49:34 +0900

    Working with UI Controls

--- LandMarks/Model/ModelData.swift
+++ LandMarks/Model/ModelData.swift
@@ -11,6 +11,7 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [LandMark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
     
     var features: [LandMark] {
         landmarks.filter{ $0.isFeatured }
